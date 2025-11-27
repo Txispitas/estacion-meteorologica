2b38e723790757ccffab16d73c93cf0ed0b3c7f9
Txispitas <txispitasmovil@gmail.com>
2025-11-27 11:57:59 +0100

    fix: Corrigiendo la ruta base de Vite para que cargue el CSS en gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// El nombre del repositorio
-const REPO_NAME = 'estacion-meteorologica'; 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Establece la base pública para la construcción (CRUCIAL para gh-pages)
-  base: `/${REPO_NAME}/`, 
+  base: "/estacion-meteorologica/", 
   plugins: [react()],
-})
+});
